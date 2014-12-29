3912ca239dfd77501ac6927aa5446f994e7e055a
edouardouvrard <edouard.ouvrard@gmail.com>
2014-12-29 18:17:01 +0100

    git instead of git-core

--- prepare-packages.sh
+++ prepare-packages.sh
@@ -3,4 +3,4 @@
 sudo apt-get update -qq
 sudo apt-get install -qq --force-yes expect
 sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch
-sudo apt-get install git-core
+sudo apt-get install git
